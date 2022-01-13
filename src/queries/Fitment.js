67c32c8d8dbe01e41400880f986ff2d46e5825d0
Colo <colo@MacBook-Pro-de-Colo.local>
2022-01-13 17:49:36 -0300

    Int! fix

--- src/queries/Fitment.js
+++ src/queries/Fitment.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export const GET_INITIAL_OPTIONS = gql`
-  query getInitialOptions ($userId: Int, $inStock: Boolean) {
+  query getInitialOptions ($userId: Int!, $inStock: Boolean) {
     getInitialOptions (userId: $userId, inStock: $inStock) {
       _id
       year
@@ -12,7 +12,7 @@ export const GET_INITIAL_OPTIONS = gql`
   `
 
 export const GET_RESULTS = gql`
-  query getResults ($userId: Int, $years: [Int], $makes: [String], $models: [String], $filter: JSONObject!, $targetOrganizationId: String, $offset: Int, $limit: Int) {
+  query getResults ($userId: Int!, $years: [Int], $makes: [String], $models: [String], $filter: JSONObject!, $targetOrganizationId: String, $offset: Int, $limit: Int) {
     getResults (userId: $userId, years: $years, makes: $makes, models: $models, filter: $filter, targetOrganizationId: $targetOrganizationId, offset: $offset, limit: $limit)
   }
 `
